dfcaa84c09df85a38d47d448211b649e940a91e7
Nickoranteng <nickoranteng@gmail.com>
2020-10-17 18:10:31 -0400

    house of pie solution updated to include bonus

--- Minis/2.04HouseOfPies-AdvancedLoops/2.04HouseOfPies_Solutions/house_of_pies_bonus.py
+++ Minis/2.04HouseOfPies-AdvancedLoops/2.04HouseOfPies_Solutions/house_of_pies_bonus.py
@@ -1,8 +1,8 @@
-# Initial variable to track shopping status
+# create variable to track shopping 
 shopping = 'y'
 
-# List to track pie purchases
-pie_purchases = []
+# track pie purchases
+pie_purchases = [0,0,0,0,0,0,0,0,0,0]
 
 # Pie List
 pie_list = ["Pecan", "Apple Crisp", "Bean", "Banoffee", "Black Bun",
@@ -14,25 +14,38 @@ print("Welcome to the House of Pies! Here are our pies:")
 # While we are still shopping...
 while shopping == "y":
 
-    # Show pie selection prompt
+    # Show pie selection prompt with indexes
     print("---------------------------------------------------------------------")
     print("(1) Pecan, (2) Apple Crisp, (3) Bean, (4) Banoffee, " +
           " (5) Black Bun, (6) Blueberry, (7) Buko, (8) Burek, " +
           " (9) Tamale, (10) Steak ")
 
-    pie_choice = input("Which would you like? ")
+    pie_choice = input("Which pie would you like to order? ")
 
-    # Add pie to the pie list
-    pie_purchases.append(pie_choice)
+    # Get index of the pie from the selected number
+    choice_index = int(pie_choice) - 1
+
+    # Add seection to the pie list by finding the matching index and adding one to its value
+    pie_purchases[choice_index] += 1
 
     print("------------------------------------------------------------------------")
 
     # Inform the customer of the pie purchase
-    print("Great! We'll have that " + pie_list[int(pie_choice) - 1] + " right out for you.")
+    print("Great! We'll have that " + pie_list[choice_index] + " right out for you.")
 
     # Provide exit option
     shopping = input("Would you like to make another purchase: (y)es or (n)o? ")
 
 # Once the pie list is complete
 print("------------------------------------------------------------------------")
-print("You purchased a total of " + str(len(pie_purchases)) + ".")
+
+# Count instances of each pie
+print("You purchased: ")
+
+# Loop through the full pie list
+for pie_index in range(len(pie_list)):
+    pie_count = str(pie_purchases[pie_index])
+    pie_name = str(pie_list[pie_index])
+
+    #  print pie count purchased
+    print(pie_count + " " + pie_name)
